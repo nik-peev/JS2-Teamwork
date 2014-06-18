db756148449af295f38768f7e043ebf655893dbb
Nikolay Peev <nikolay.peev@gmail.com>
2014-06-18 23:20:55 +0300

    Bonuses upgraded

--- scripts/Bonus.js
+++ scripts/Bonus.js
@@ -9,8 +9,15 @@ function createMovingBonus( destinationX, destinationY, bonus){
     console.log('Type:' + bonus.type);
     console.log((destinationX - bonus.x)/100);
     movingBonuses.push({ img: bonus.bonusAsKineticObj,
+        x:bonus.x,
+        y:bonus.y,
+        type:bonus.type,
+        health:bonus.health,
+        boost:bonus.boost,
         speedX: (destinationX - bonus.x)/100,
-        speedY: (destinationY - bonus.y)/100
+        speedY: (destinationY - bonus.y)/100,
+        destinationX: destinationX,
+        destinationY: destinationY
     });
 
 }
@@ -87,7 +94,33 @@ function isPlayerOnBonus(x, y, radius, strokeWidth) {
 
 var moveBonuses = new Kinetic.Animation(function (frame) {
     for (var i = 0; i < movingBonuses.length; i++) {
-        movingBonuses[i].img.move({x:movingBonuses[i].speedX,y:movingBonuses[i].speedY})
+        if (movingBonuses[i].img) {
+            movingBonuses[i].img.move({x:movingBonuses[i].speedX,y:movingBonuses[i].speedY});
+            movingBonuses[i].x = movingBonuses[i].x + movingBonuses[i].speedX;
+            movingBonuses[i].y = movingBonuses[i].y + movingBonuses[i].speedY;
+//            console.log(movingBonuses[i].x);
+//            console.log(movingBonuses[i].y);
+            if (movingBonuses[i].x < movingBonuses[i].destinationX || movingBonuses[i].y < movingBonuses[i].destinationY){
+                movingBonuses[i].img.destroy();
+                movingBonuses[i].img = null;
+
+                if (movingBonuses[i].type === 'health') {
+                    document.getElementById("bonus").cloneNode(true).play();
+                    if (health.getWidth() + movingBonuses[i].health > 100) {
+                        health.setWidth(100);
+                    }
+                    else {
+                        health.setWidth(health.getWidth() + movingBonuses[i].health);
+                    }
+                }
+                if (movingBonuses[i].type === 'score-boost') {
+                    document.getElementById("medikit").cloneNode(true).play();
+                    bonusPoints += movingBonuses[i].boost;
+                }
+
+                movingBonuses.splice(i, 1);
+            }
+        }
     }
 }, layer);
 
@@ -98,20 +131,10 @@ var updateBonus = new Kinetic.Animation(function (frame) {
                 if (isPlayerOnBonus(allBonusObjects[i].x, allBonusObjects[i].y,
                     allBonusObjects[i].radius, allBonusObjects[i].strokeWidth)) {
                     if (allBonusObjects[i].type === 'health') {
-                        createMovingBonus(0,0,allBonusObjects[i]);
-                        if (health.getWidth() + allBonusObjects[i].health > 100) {
-                            health.setWidth(100);
-                            document.getElementById("bonus").cloneNode(true).play();
-                        }
-                        else {
-                            health.setWidth(health.getWidth() + allBonusObjects[i].health);
-                            document.getElementById("bonus").cloneNode(true).play();
-                        }
+                        createMovingBonus(100,20,allBonusObjects[i]);
                     }
                     if (allBonusObjects[i].type === 'score-boost') {
-                        bonusPoints += allBonusObjects[i].boost;
-                        createMovingBonus(0,0,allBonusObjects[i]);
-                        document.getElementById("medikit").cloneNode(true).play();
+                        createMovingBonus(170,20,allBonusObjects[i]);
                     }
 
                     allBonusObjects.splice(i, 1);
